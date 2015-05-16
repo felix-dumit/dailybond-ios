e5da1b319bf2aae2dc8419405d8dc564a3b6a877
nickmm <nickmm@mit.edu>
2015-05-16 16:55:04 -0300

    Adding birthday cards

---
+++ DailyBond/DailyBond/CardBirthdayViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardBirthdayViewController.h
+//  DailyBond
+//
+//  Created by Nick-Retina on 5/16/15.
+//  Copyright (c) 2015 DailyBond. All rights reserved.
+//
+
+#import "CardViewController.h"
+
+@interface CardBirthdayViewController : CardViewController
+
+@end
