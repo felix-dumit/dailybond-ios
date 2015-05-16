34a8069ce148ddaefe42674c46f299ff4e0ae6d3
Fabio Perez <fabiovmp@gmail.com>
2015-05-16 18:47:04 -0300

    Log friends when button is clicked

--- DailyBond/DailyBond/FBRequests.swift
+++ DailyBond/DailyBond/FBRequests.swift
@@ -10,8 +10,17 @@ import Foundation
 
 @objc class FBRequests {
     
-    func doSomething() -> Int {
-        return 2
+    static let sharedInstance = FBRequests()
+    
+    func doSomething() -> Void {
+    
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            FBSDKGraphRequest(graphPath: "me/friends", parameters: ["fields" : "id,name,birthday"]).startWithCompletionHandler({ (connection, data, error) -> Void in
+                println(data)
+                println(error)
+            })
+        }
+        
     }
     
     func formatString(name: String) -> String {
