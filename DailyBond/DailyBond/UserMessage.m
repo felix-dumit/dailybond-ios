e97db163ec212d0183ce274d2c0db46f564824f7
Fabio Perez <fabiovmp@gmail.com>
2015-05-17 03:41:14 -0300

    Add UserMessage

--- DailyBond/DailyBond/UserMessage.m
+++ DailyBond/DailyBond/UserMessage.m
@@ -9,15 +9,83 @@
 #import "UserMessage.h"
 #import <Parse/Parse.h>
 
+@interface UserMessage ()
+
+@property (strong, nonatomic) NSString *dateString;
+@end
+
 @implementation UserMessage
 @dynamic friendId;
 @dynamic friendName;
 @dynamic receivedDate;
 @dynamic message;
-@dynamic read;
-@dynamic profileImageUrl;
+@dynamic unread;
+@dynamic dateString;
+
 
 + (NSString *)parseClassName {
     return @"UserMessage";
+}
+
+- (void)setDateString:(NSString *)dateString {
+    [self setObject:dateString forKey:dateString];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    [dateFormatter setLocale:enUSPOSIXLocale];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+    NSDate *date = [dateFormatter dateFromString:dateString];
+    self.receivedDate = date;
+}
+
++ (void)registerSubclass {
+    [super registerSubclass];
+    
+    [UserMessage setupReplacedKeyFromPropertyName: ^NSDictionary *{
+        return @{
+                 @"friendId": @"id.to.data.id",
+                 @"friendName" : @"id.to.data.name",
+                 @"receivedDate": @"updated_time",
+                 @"message": @"comments.data.message",
+                 @"read": @"unread"
+                 };
+    }];
+}
+
++ (instancetype)createWithName:(NSString *)friendName andId:(NSString *)friendId  andDate:(NSDate *)receivedDate andMessage:(NSString *)message andUnread:(BOOL)unread {
+    UserMessage *user_msg = [UserMessage object];
+    user_msg.friendId = friendId;
+    user_msg.friendName = friendName;
+    user_msg.receivedDate = receivedDate;
+    user_msg.message = message;
+    user_msg.unread = unread;
+    return user_msg;
+}
+
++ (UserMessage *)generateMockData {
+    return [self createWithName:@"Jorge" andId:@"1" andDate:[NSDate dateWithTimeInterval:-100000000 sinceDate:[NSDate date]] andMessage:@"Hello!" andUnread:YES];
+}
+
++ (BFTask *)allMessages {
+    static BFTask *currentTask = nil;
+    
+    if (!currentTask) {
+        currentTask = [[FBRequests sharedInstance] getInbox].then ( ^id (NSArray *result) {
+            return [UserMessage objectArrayWithKeyValuesArray:result];
+        }).catch ( ^id (NSError *error) {
+            return [self generateMockData];
+        });
+    }
+    
+    return currentTask;
+}
+
+- (NSArray *)createEventsArrayFromJSONArray:(NSArray *)array {
+    NSArray *arr = [UserMessage objectArrayWithKeyValuesArray:array];
+    return arr;
+}
+
+-(NSURL*)profileImageURL {
+    return [NSURL URLWithString:[NSString stringWithFormat:@"graph.facebook.com/%@/picture?type=square", self.friendId]];
 }
 @end
